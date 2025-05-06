572424e46868ea169fb0870bdbf992bc3b0d98ad
Jeelsavaliya <jeelsavaliya007@gmail.com>
2025-05-06 19:17:48 +0530

    again new line added in branch1

--- GitLearningDemo/Program.cs
+++ GitLearningDemo/Program.cs
@@ -2,5 +2,6 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, This is branch 1 :)");
 Console.WriteLine("Hello, This new changes in branch 1 :)");
+Console.WriteLine("Hello, This another new changes in branch 1 :)");
 Console.WriteLine("Hello, This is branch 2 :)");
 
