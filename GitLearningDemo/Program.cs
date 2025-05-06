6a2d7ecad1da151449118b353bdb5e3a34e4c7b7
Jeelsavaliya <jeelsavaliya007@gmail.com>
2025-05-06 19:14:25 +0530

    new line added in branch1

--- GitLearningDemo/Program.cs
+++ GitLearningDemo/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, This is branch 1 :)");
+Console.WriteLine("Hello, This new changes in branch 1 :)");
 Console.WriteLine("Hello, This is branch 2 :)");
 
-Console.WriteLine("Hello, This is branch 1 :)");
